39fc2e36704c670a322962068ccaeb49397b564d
GauravDhanuka <gauravdhanuka2007@gmail.com>
2015-06-14 09:33:27 +0530

    Done with static pages

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    if Rails.env.test?
+        initializer :after => :initialize_dependency_mechanism do
+            # Work around initializer in railties/lib/rails/application/bootstrap.rb
+            ActiveSupport::Dependencies.mechanism = :load
+        end
+    end
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, :type => :controller do
-
+  render_views
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should_have_selector('title', content: "Sample App | Home")
     end
   end
 
@@ -14,6 +20,23 @@ RSpec.describe PagesController, :type => :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should_have_selector('title', content: "Sample App | Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should_have_selector('title', content: "Sample App | About")
+    end
+  end
 end
+
